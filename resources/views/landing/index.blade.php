6cddbb6092db3a142b5c69518113874852227ef7
EshanDev <theerapong.b@ru.ac.th>
2020-11-25 15:40:56 +0000

    Add Favicon image

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Welcome to Self-training.in.th</title>
+</head>
+<body>
+    <h1 style="text-align: center">Welcome to Self-training.in.th</h1>
+</body>
+</html>
